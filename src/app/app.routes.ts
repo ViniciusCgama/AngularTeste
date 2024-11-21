a08380d2caf15900039bf06b0da4496a6d1360a6
Vinicius Coelho Gama <coelhovinigama@gmail.com>
2024-11-21 14:14:26 -0300

    Atualizão front end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'footer', component: FooterComponent}
+
 ]
 ;
